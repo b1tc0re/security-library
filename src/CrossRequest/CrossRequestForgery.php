0499e66a387ecbfe2d20dfb95411bdb9db64da89
b1tc0re <b1tc0re@yandex.ru>
2022-05-23 03:51:53 +0300

    message

--- src/CrossRequest/CrossRequestForgery.php
+++ src/CrossRequest/CrossRequestForgery.php
@@ -1,9 +1,10 @@
 <?php  namespace DeftCMS\Components\b1tc0re\Security\CrossRequest;
 
 use DeftCMS\Components\b1tc0re\Security\CrossRequest\Interfaces\ICrossRequestForgery;
+use DeftCMS\Core\RequestNotify;
 use DeftCMS\Engine;
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') || exit('No direct script access allowed');
 
 /**
  * Зашита от межсайтовой атак
@@ -12,8 +13,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @package	    DeftCMS
  * @category	Library
  * @author	    b1tc0re
- * @copyright   (c) 2018-2020, DeftCMS (http://deftcms.ru/)
- * @since	    Version 0.0.9
+ * @copyright   (c) 2018-2022, DeftCMS (http://deftcms.ru/)
+ * @since	    Version 0.0.9a
  */
 class CrossRequestForgery implements ICrossRequestForgery
 {
@@ -155,10 +156,16 @@ class CrossRequestForgery implements ICrossRequestForgery
 
                 Engine::$DT->validation->setData('cross_request_forgery', true);
                 Engine::$DT->validation->setData('task', [
-                    'updateToken' =>$cookie_token
+                    'updateToken' => $cookie_token
                 ]);
+                RequestNotify::getInstance()->messages(
+                    __('Language::Error::cross_request_forgery'),
+                    \DeftCMS\Core\RequestNotify::ERROR
+                );
+
 
                 Engine::$DT->validation->output();
+
                 exit;
             }
 
